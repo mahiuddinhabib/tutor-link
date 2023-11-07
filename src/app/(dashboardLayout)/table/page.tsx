c998a157e3186e2f7453193361973d856e0112dd
mahiuddinhabib <mh.habib137@gmail.com>
2023-11-07 17:29:13 +0600

    reusable table with pagination done

---
+++ src/app/(dashboardLayout)/table/page.tsx
@@ -0,0 +1,12 @@
+import PaginatedTable from "@/components/ui/TableWithPagination";
+import React from "react";
+
+const Table = () => {
+  return (
+    <div>
+      <PaginatedTable />
+    </div>
+  );
+};
+
+export default Table;
